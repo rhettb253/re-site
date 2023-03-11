520773bda058a4c3c53166efe6f8477a79e35c92
rhettb253 <rhettbeardemphl@hotmail.com>
2023-03-10 17:05:06 -0800

    get connect buyer form and some of seller form

--- src/app/App.js
+++ src/app/App.js
@@ -5,23 +5,158 @@ import AboutMe from '../aboutme/AboutMe';
 import GetConnected from '../getconnected/GetConnected';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.changeClientWants = this.changeClientWants.bind(this);
+    this.updateBuyerInfo = this.updateBuyerInfo.bind(this);
     this.state = {
-        num : 0,
-        wantsTo: '',
-
+      clientWantsTo: '',
+      buyer: {
+        fullName: '',
+        emailAddress: '',
+        isInterestedInAHouse: '',
+        isInterestedInATownhome: '',
+        isInterestedInACondo: '',
+        isInterestedInAMultifamily: '',
+        isWillingToSpend: '',
+        bedrooms: '',
+        bathrooms: '',
+        desiredAreaToLive: '',
+        extraInformation: '',
+        timeframe: ''
+      },
+      seller: {
+        fullName: '',
+        emailAddress: '',
+        isSellingAHouse: '',
+        isSellingATownhome: '',
+        isSellingACondo: '',
+        isSellingAMultifamily: '',
+        believesHomeValueIs: '',
+        sqft: '',
+        lot: '',
+        bedrooms: '',
+        bathrooms: '',
+        addressOfHome: '',
+        extraInformation: '',
+        timeframe: ''
       }
+    }
   }
-  render(){
+
+  changeClientWants(buysell) {
+    this.setState({
+      clientWantsTo: buysell
+    });
+  }
+
+  updateBuyerInfo(event) {
+    event.preventDefault();
+    if (
+      event.target.fullName.value === "" ||
+      event.target.email.value === "" ||
+      event.target.price.value === "" ||
+      event.target.beds.value === "" ||
+      event.target.baths.value === "" ||
+      event.target.city.value === "" ||
+      event.target.timeframe.value === "") {
+      if (!document.getElementById('warning')) {
+        let location = document.querySelector('.buyer');
+        let formWarningEl = document.createElement('h3');
+        formWarningEl.id = 'warning';
+        formWarningEl.innerText = 'Please review the form and fill in any missing information.';
+        location.appendChild(formWarningEl);
+      } else {
+        document.getElementById('warning').innerText = 'Please review the form as there is still missing information.';
+      }
+    } else if (event.target.timeframeTextArea) {
+      this.setState({
+        buyer: {
+          fullName: event.target.fullName.value,
+          emailAddress: event.target.email.value,
+          isInterestedInAHouse: event.target.house.checked ? 'Yes' : 'No',
+          isInterestedInATownhome: event.target.townhome.checked ? 'Yes' : 'No',
+          isInterestedInACondo: event.target.condo.checked ? 'Yes' : 'No',
+          isInterestedInAMultifamily: event.target.multifamily.checked ? 'Yes' : 'No',
+          isWillingToSpend: event.target.price.value,
+          bedrooms: event.target.beds.value,
+          bathrooms: event.target.baths.value,
+          desiredAreaToLive: event.target.city.value,
+          extraInformation: event.target.extraInfo.value,
+          timeframe: event.target.timeframe.value,
+          timeframeInfo: event.target.timeframeTextArea.value
+        }
+      });
+      if (document.getElementById('warning')) {
+        let formWarningEl = document.getElementById('warning');
+        formWarningEl.remove();
+        let locationTwo = document.querySelector('.buyer');
+        let thankYouEl = document.createElement('h3');
+        thankYouEl.id = 'thankYou';
+        thankYouEl.innerText = 'Thank you for your submission. I look forward to connecting with you soon!';
+        locationTwo.appendChild(thankYouEl);
+      } else {
+          let locationTwo = document.querySelector('.buyer');
+          let thankYouEl = document.createElement('h3');
+          thankYouEl.id = 'thankYou';
+          thankYouEl.innerText = 'Thank you for your submission. I look forward to connecting with you soon!';
+          locationTwo.appendChild(thankYouEl);
+      }
+    } else {
+      this.setState({
+        buyer: {
+          fullName: event.target.fullName.value,
+          emailAddress: event.target.email.value,
+          isInterestedInAHouse: event.target.house.checked ? 'Yes' : 'No',
+          isInterestedInATownhome: event.target.townhome.checked ? 'Yes' : 'No',
+          isInterestedInACondo: event.target.condo.checked ? 'Yes' : 'No',
+          isInterestedInAMultifamily: event.target.multifamily.checked ? 'Yes' : 'No',
+          isWillingToSpend: event.target.price.value,
+          bedrooms: event.target.beds.value,
+          bathrooms: event.target.baths.value,
+          desiredAreaToLive: event.target.city.value,
+          extraInformation: event.target.extraInfo.value,
+          timeframe: event.target.timeframe.value
+        }
+      });
+      if (document.getElementById('warning')) {
+        let formWarningEl = document.getElementById('warning');
+        formWarningEl.remove();
+        let locationTwo = document.querySelector('.buyer');
+        let thankYouEl = document.createElement('h3');
+        thankYouEl.id = 'thankYou';
+        thankYouEl.innerText = 'Thank you for your submission. I look forward to connecting with you soon!';
+        locationTwo.appendChild(thankYouEl);
+      } else {
+          let locationTwo = document.querySelector('.buyer');
+          let thankYouEl = document.createElement('h3');
+          thankYouEl.id = 'thankYou';
+          thankYouEl.innerText = 'Thank you for your submission. I look forward to connecting with you soon!';
+          locationTwo.appendChild(thankYouEl);
+      }
+    }
+  }
+
+  render() {
     return (
       <div className='app'>
         <Header />
         <AboutMe />
-        <GetConnected wantsTo={this.state.wantsTo}/>
+        <GetConnected
+          clientWantsTo={this.state.clientWantsTo}
+          onChange={this.changeClientWants}
+          buyerInfo={this.state.buyer}
+          newBuyerHandler={this.updateBuyerInfo}
+          sellerInfo={this.state.seller} />
         {/*<Footer /> */}
       </div>
     );
+  }
+
+  componentDidUpdate(){
+    if (document.getElementById('thankYou')){
+      document.getElementById('buyerButton').remove();
+    }
   }
 }
 
--- src/getconnected/GetConnected.js
+++ src/getconnected/GetConnected.js
@@ -1,40 +1,52 @@
 import React from 'react';
+import Buyer from '../buyer/Buyer';
+import Seller from '../seller/Seller';
 import './GetConnected.css';
 
-class GetConnected extends React.Component{
-    constructor (props) {
+class GetConnected extends React.Component {
+    constructor(props) {
         super(props);
-        this.introStatement =
-        <div>
-                   
-        </div>
+        this.handleChange = this.handleChange.bind(this);
+        this.createForm = this.createForm.bind(this);
     }
-    render(){
-        return(
+
+    handleChange(event) {
+        const buysell = event.target.value;
+        this.props.onChange(buysell)
+    }
+
+    createForm() {
+        if (this.props.clientWantsTo === "buy") {
+            let form = <Buyer
+                buyerInfo={this.props.buyerInfo}
+                newBuyerHandler={this.props.newBuyerHandler} />;
+            return form;    
+        } else if (this.props.clientWantsTo === 'sell') {
+            let form = <Seller 
+                sellerInfo={this.props.sellerInfo} />;
+            return form;
+        }
+    }
+
+    render() {
+        return (
             <div className='getconnected'>
                 <h2>Let's Get Connected!</h2>
                 <div className="question1">
                     <h3>Tell me why you are here:</h3>
-                    <p>I am interested in 
-                        <select name="introInput" id="introInput">
-                            <option>select</option>
+                    <p>I am interested in
+                        <select name="introInput" id="introInput" onChange={this.handleChange}>
+                            <option value={null}>buying/selling</option>
                             <option value="sell">selling</option>
                             <option value="buy">buying</option>
                         </select>
                     my home.</p>
                 </div>
+                {this.createForm()}
             </div>
-            )
-        }
-            // <form>
-            //     <fieldset>
-            //         <legend>
-            //         </legend>
-            //     </fieldset>
-            // </form>
-    // componentDidMount(){
-        
-    // }
+        )
+    }
+
 }
 
 export default GetConnected;
