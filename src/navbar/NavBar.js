18d0915a58acf359318e5f1bed4c9cf6bd12dc95
rhettb253 <rhettbeardemphl@hotmail.com>
2023-03-14 14:33:25 -0700

    fix anchors for netlify

--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -5,9 +5,9 @@ class NavBar extends React.Component {
     render(){
         return(
             <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#">PREVIOUS TRANSACTIONS</a></li>
-                <li><a href="#">FIRST TIME BUYER RESOURCES</a></li>
+                <li><a href="www.google.com">HOME</a></li>
+                <li><a href="www.google.com">PREVIOUS TRANSACTIONS</a></li>
+                <li><a href="www.google.com">FIRST TIME BUYER RESOURCES</a></li>
             </ul>
         )
     }
