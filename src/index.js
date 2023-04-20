8b9b30d59efe2f50362dc2db7f3a214f7c1f4e04
rhettb253 <rhettbeardemphl@hotmail.com>
2023-04-19 19:53:23 -0700

    started bootstrap and rounded borders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Bridge from './bridge';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
