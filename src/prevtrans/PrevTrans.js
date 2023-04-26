bef388b2213e306e0a7b15c75863c94a8e8780e5
rhettb253 <rhettbeardemphl@hotmail.com>
2023-04-25 17:14:08 -0700

    almost done entering data!

--- src/prevtrans/PrevTrans.js
+++ src/prevtrans/PrevTrans.js
@@ -1,12 +1,34 @@
 import React from "react";
+// import Card from "react-bootstrap/Card";
 import './PrevTrans.css';
 
-class PrevTrans extends React.Component{
+class PrevTrans extends React.Component {
     render() {
-        return(
-            <>
-                
-            </>
+        return (
+            <div className="prevtrans">
+                <h2>Previous Transactions</h2>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <img src={require("../assets/properties/2731.webp")} />
+                            {/* <div class="card" style={{width: "18rem"}}>
+                                <img src="..." class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                            </div> */}
+                        </div>
+                        <div className="col">
+                            Column
+                        </div>
+                        <div className="col">
+                            Column
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
